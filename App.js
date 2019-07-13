a64981a67400ef55873364cf1401e9d29552e91c
dilrong <wet932@naver.com>
2019-07-14 00:10:02 +0900

    bottombar custom

--- App.js
+++ App.js
@@ -1,15 +1,68 @@
 import React from 'react';
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 import { MapScreen, ListScreen, BoardScreen } from './screens/index';
+import { MaterialCommunityIcons, Ionicons } from '@expo/vector-icons';
 
 const TapNavigator = createBottomTabNavigator({
-  Map: MapScreen,
-  List: ListScreen,
-  Board: BoardScreen
+  Map: {
+    screen: MapScreen,
+    navigationOptions: () => ({
+      tabBarIcon: () => (
+        <MaterialCommunityIcons
+        name='map-marker'
+        color='white'
+        size={24}
+        />
+      )
+    })
+  },
+  List: {
+    screen: ListScreen,
+    navigationOptions: () => ({
+      tabBarIcon: () => (
+        <MaterialCommunityIcons
+        name='format-list-bulleted'
+        color='white'
+        size={24}
+        />
+      )
+    })
+  },
+  Board: {
+    screen: BoardScreen,
+    navigationOptions: () => ({
+      tabBarIcon: () => (
+        <Ionicons
+        name='ios-chatboxes'
+        color='white'
+        size={24}
+        />
+      )
+    })
+  },
+  ETC: {
+    screen: BoardScreen,
+    navigationOptions: () => ({
+      tabBarIcon: () => (
+        <MaterialCommunityIcons
+        name='dots-horizontal'
+        color='white'
+        size={24}
+        />
+      )
+    })
+  }
 }, {
   initialRouteName: 'Map',
   header: null,
-  headerMode: 'none'
+  headerMode: 'none',
+  tabBarOptions: {
+    activeTintColor: '#ffffff',
+    inactiveTintColor: '#bdc3c7',
+    style: {
+      backgroundColor: '#12191d'
+    }
+  }
 });
 
 const AppContainer = createAppContainer(TapNavigator);
